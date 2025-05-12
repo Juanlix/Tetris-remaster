dfb4fe5a127a8162d23f171a5ce215fb3d7e6574
Juanlix <sans34696@gmail.com>
2025-05-12 12:46:38 -0300

    funcion sin conccionx

--- Tetris-master/sw.js
+++ Tetris-master/sw.js
@@ -1,21 +1,64 @@
-const CACHE = "v17";
-const urlsToCache =[
+// Nombre y versión del caché
+const CACHE_NAME = "v18";
+const urlsToCache = [
+  './',
+  './dist/tetris.js',
+  './css/styles.css',
+  './index.html',
+  './manifest.json',
+  './icons/192.png',
+  './icons/512.png'
+];
 
-  '/',
-  '/dist/tetris.js',
-  '/css/styles.css',
-  '/index.html',
-  
-
-
-
-]
-self.addEventListener('install', async (event) => {
+// Instalación del Service Worker y cacheo de archivos
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
-
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.postMessage("SW instalado y cacheado"));
+  });
 });
 
+// Intercepta las peticiones y sirve desde el caché
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          self.clients.matchAll().then(clients => {
+            clients.forEach(client => client.postMessage(`Recurso cargado desde caché: ${event.request.url}`));
+          });
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch((err) => {
+        self.clients.matchAll().then(clients => {
+          clients.forEach(client => client.postMessage(`Error al cargar el recurso: ${event.request.url}`));
+        });
+      })
+  );
+});
 
+// Limpieza de cachés antiguos al activar un nuevo Service Worker
+self.addEventListener('activate', (event) => {
+  const whitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!whitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.matchAll().then(clients => {
+    clients.forEach(client => client.postMessage("SW activado y cachés antiguos eliminados"));
+  });
+});
